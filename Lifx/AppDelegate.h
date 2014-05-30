5fc6c2256d0e81c9712dc16e07580063ada187ac
dspoka <dspoka@gmail.com>
2014-05-30 15:52:03 -0700

    Initial Commit

---
+++ Lifx/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lifx
+//
+//  Created by Daniel Spokoyny on 5/30/14.
+//  Copyright (c) 2014 Daniel Spokoyny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lifx/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lifx
+//
+//  Created by Daniel Spokoyny on 5/30/14.
+//  Copyright (c) 2014 Daniel Spokoyny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
